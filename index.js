d9b49d7fd3e6d3366ff39cab5ef4da6257c27d5c
Sergey <jetFighterrr>
2019-03-03 03:37:20 -0800

    project is updated

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
+import {read, update, remove, create, edit} from './src/responseManager'
 
 app.use(morgan('dev'));
 
-app.get('/',(req,res) => {
+//get read here
+app.get('/read',(req,res) => {
   // res.send('Hello');
   res.status(200).json({message: 'OK', title:'Hello OK'});
 });
 
-app.get('/1',(req,res) => {
-  res.send('Fuck off')
+//put edit here
+app.put('/edit',(req,res) => {
+  res.send(edit())
 });
 
+//post update here
 app.post('/post',(req,res) => {
-  res.status(200).json({message: 'OK', title:'Hello OK'});
+  res.status(200).json({message: update(), title:'Hello OK'});
 });
 
+//post create here
+app.post('/post',(req,res) => {
+  res.status(200).json({message: remove(), title:'Hello OK'});
+});
 
-app.listen(5000, ()=>console.log('LISTEN 5000'));
+//delete remove here()
+app.post('/post',(req,res) => {
+  res.status(200).json({message: remove(), title:'Hello OK'});
+});
+
+// app.post('/edit',(req,res) => {
+//   res.send(edit())
+// });
+
+
+app.listen(5000, () => console.log('LISTEN 5000'));
