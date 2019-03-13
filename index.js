09ea13430eee63837efefb08c267c325a360fa91
Sergey <jetFighterrr>
2019-03-13 08:40:23 -0700

    project is updated

--- index.js
+++ index.js
@@ -1,39 +1,69 @@
+'use strict';
+
+const responseManager = require('./src/responseManager');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
-import {read, update, remove, create, edit} from './src/responseManager'
+
+// import {edit} from './src/responseManager';
+
+console.log(edit());
 
 app.use(morgan('dev'));
+// app.require('http');
+
 
 //get read here
 app.get('/read',(req,res) => {
-  // res.send('Hello');
+  res.send('Hello');
   res.status(200).json({message: 'OK', title:'Hello OK'});
 });
 
 //put edit here
-app.put('/edit',(req,res) => {
-  res.send(edit())
+app.get('/edit',(req,res) => {
+  res.status(200).send(edit())
 });
 
 //post update here
-app.post('/post',(req,res) => {
-  res.status(200).json({message: update(), title:'Hello OK'});
-});
+// app.post('/post',(req,res) => {
+//   res.status(200).json({message: update(), title:'Hello OK'});
+// });
 
 //post create here
-app.post('/post',(req,res) => {
-  res.status(200).json({message: remove(), title:'Hello OK'});
-});
+// app.post('/post',(req,res) => {
+//   res.status(200).json({message: create(), title:'Hello OK'});
+// });
 
 //delete remove here()
-app.post('/post',(req,res) => {
-  res.status(200).json({message: remove(), title:'Hello OK'});
-});
+// app.post('/post',(req,res) => {
+//   res.status(200).json({message: remove(), title:'Hello OK'});
+// });
 
 // app.post('/edit',(req,res) => {
 //   res.send(edit())
 // });
 
 
-app.listen(5000, () => console.log('LISTEN 5000'));
+app.listen(5000, () => console.log('LISTEN to 5000'));
+
+// 'use strict';
+// export {edit} ;
+
+function edit(){
+  return "edit";
+}
+function read(){
+  return "read"
+}
+
+function update(){
+  return "update";
+}
+
+function create(){
+  return "create";
+}
+
+function remove() {
+  return "remove";
+}
